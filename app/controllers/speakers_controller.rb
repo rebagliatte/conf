a8c7a3da3dce44fddec191df6b60d0b8b14f2a02
Veronica Rebagliatte <rebagliatte@gmail.com>
2013-05-01 16:14:34 -0300

    Order speakers by name

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,5 +1,5 @@
 class SpeakersController < ApplicationController
   def index
-    @speakers = ConferenceEdition.find(params[:conference_edition_id]).speakers
+    @speakers = ConferenceEdition.find(params[:conference_edition_id]).speakers.order('name ASC')
   end
 end
