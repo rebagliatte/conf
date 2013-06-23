1857f84b4b48a40ce5a6f3d659cb7382e2f77d17
Veronica Rebagliatte <rebagliatte@gmail.com>
2013-06-23 20:57:13 -0300

    New superadmin role and manageable editions method on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ class User < ActiveRecord::Base
 
   has_many :identities
   has_one :speaker
-  has_and_belongs_to_many :conferences
-  has_many :conferences
+  has_and_belongs_to_many :manageable_conferences, class_name: 'Conference'
+  has_many :owned_conferences, class_name: 'Conference', foreign_key: :owner_id
 
-  ROLES = %w( user admin )
+  ROLES = %w( user admin superadmin )
 
   validates :name, presence: true
   validates :role, inclusion: { in: ROLES }
+
+  def superadmin?
+    self.role == 'superadmin'
+  end
 
   def admin?
     self.role == 'admin'
@@ -17,5 +21,13 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(info)
     create!(name: info['name'], nickname: info['nickname'], email: info['email'], image: info['image'])
+  end
+
+  def manageable_conference_editions
+    conference_editions = []
+    Conference.where(owner_id: id).each do |c|
+      conference_editions << c.conference_editions
+    end
+    conference_editions.flatten
   end
 end
