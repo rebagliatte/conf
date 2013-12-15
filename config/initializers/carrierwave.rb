9546250622eb886c2ca0eede45bd7d389b9edd65
Vero Rebagliatte <veronica@rebagliatte.com>
2013-12-15 00:47:29 -0200

    Update carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,5 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: CONFIG[:aws_access_key_id],
-    aws_secret_access_key: CONFIG[:aws_secret_access_key]
-  }
-  config.fog_directory = CONFIG[:aws_s3_bucket]
+  config.s3_access_key_id = CONFIG[:aws_access_key_id]
+  config.s3_secret_access_key = CONFIG[:aws_secret_access_key]
+  config.s3_bucket = CONFIG[:aws_s3_bucket]
 end
