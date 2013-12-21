3d07a51822f46d17577795c7870964dd4a08fc7f
Vero Rebagliatte <veronica@rebagliatte.com>
2013-12-21 15:35:03 -0200

    Add ability to download a subscriber CSV

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -7,4 +7,13 @@ class Subscriber < ActiveRecord::Base
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
   validates :email, presence: true, format: EMAIL_REGEX
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |subscriber|
+        csv << subscriber.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
