a078ac49b4be40d0b914a8672c002868c552196f
Vero Rebagliatte <veronica@rebagliatte.com>
2013-12-21 15:48:44 -0200

    Enable and disable subscribers, translatable copy for inviting them to subscribe

--- app/models/conference_edition.rb
+++ app/models/conference_edition.rb
@@ -1,8 +1,11 @@
 class ConferenceEdition < ActiveRecord::Base
-  attr_accessible :from_date, :to_date, :kind, :promo_video_provider, :promo_video_uid, :promo_image, \
-  :logo, :conference, :conference_id, :translations_attributes, :tagline, :country, :city, :venue, \
-  :sponsorship_packages_pdf, :registration_url, :is_registration_open, :is_call_for_proposals_open, \
-  :is_call_for_sponsorships_open, :is_schedule_available, :is_location_available, :notes_to_speakers
+  attr_accessible :from_date, :to_date, :kind, :promo_video_provider, \
+  :promo_video_uid, :promo_image, :logo, :conference, :conference_id, \
+  :translations_attributes, :tagline, :country, :city, :venue, \
+  :sponsorship_packages_pdf, :registration_url, :is_registration_open, \
+  :is_call_for_proposals_open, :is_call_for_sponsorships_open, \
+  :is_schedule_available, :is_location_available, :notes_to_speakers, \
+  :is_email_subscription_enabled, :notes_to_subscribers
 
   belongs_to :conference
   has_many :posts, dependent: :destroy
@@ -38,7 +41,8 @@ class ConferenceEdition < ActiveRecord::Base
   # Translations
   has_many :translations
   accepts_nested_attributes_for :translations
-  translates :tagline, :country, :city, :venue, :notes_to_speakers
+  translates :tagline, :country, :city, :venue, :notes_to_speakers, \
+    :notes_to_subscribers
 
   # Uploaders
   mount_uploader :logo, ImageUploader
