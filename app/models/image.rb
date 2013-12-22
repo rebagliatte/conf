b699e8c733720d709228d865585041aead28ffb4
Vero Rebagliatte <veronica@rebagliatte.com>
2013-12-22 17:59:37 -0200

    Establish a maximum for image uploads per edition

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,18 @@ class Image < ActiveRecord::Base
   belongs_to :conference_edition
   has_one :conference, through: :conference_edition
 
+  MAX_IMAGES_PER_EDITION = 10
+
   # Validations
   validates :image, presence: true
+  validate :edition_images_length
+
+  def edition_images_length
+    message = "can't be more than #{MAX_IMAGES_PER_EDITION} for a given conference edition."
+    if conference_edition.images.length > MAX_IMAGES_PER_EDITION
+      errors.add(:images, message)
+    end
+  end
 
   # Uploaders
   mount_uploader :image, ImageUploader
