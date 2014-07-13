6497bfe69099b8cf770b1ca61820e1da964adca4
Vero Rebagliatte <veronica@rebagliatte.com>
2014-07-13 16:24:33 -0300

    Set linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :deploy_to, '~/apps/conf'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Set ssh user
 set :user, 'deploy'
@@ -39,26 +39,3 @@ set :ssh_options, {
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-# namespace :deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :publishing, :restart
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
