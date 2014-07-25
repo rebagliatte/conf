62aca7b571809f0096bd7eed2a0e4894aac7e9d7
Vero Rebagliatte <veronica@rebagliatte.com>
2014-07-25 18:29:58 -0300

    Fix typo

--- lib/root_constraint.rb
+++ lib/root_constraint.rb
@@ -1,5 +1,5 @@
 class RootConstraint
   def matches?(request)
-    Rails.application.secrets.confnu_server_names].split(' ').include?(request.domain) && request.subdomain.empty?
+    Rails.application.secrets.confnu_server_names.split(' ').include?(request.domain) && request.subdomain.empty?
   end
 end
