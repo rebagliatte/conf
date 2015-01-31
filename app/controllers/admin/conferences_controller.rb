5684bb17a8019f0fa9538bbe0cb0d5c14db5d2c6
Vero Rebagliatte <veronica@rebagliatte.com>
2015-01-31 20:15:17 -0200

    Add subdomain to conference creation redirect

--- app/controllers/admin/conferences_controller.rb
+++ app/controllers/admin/conferences_controller.rb
@@ -21,7 +21,7 @@ class Admin::ConferencesController < AdminController
       first_edition = @conference.conference_editions.first
       first_edition.organizers << current_user
 
-      redirect_url = admin_conference_edition_organizers_path(first_edition)
+      redirect_url = admin_conference_edition_organizers_url(first_edition, subdomain: @conference.subdomain)
       message = "Conference created successfully! Go ahead and invite in some co-organizers."
       redirect_to redirect_url, flash: { success: message }
     else
